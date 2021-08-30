8fdd938353cd20d89769b882554f055980892d97
Tran Xuan Huong <phamhyta@gmail.com>
2021-08-30 12:52:52 +0700

    --- Auto-Git Commit ---

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
                 currentPos = 0;
             img.src = imgs[currentPos];
         }
-        setInterval(volgendefoto, 3000);
+        setInterval(volgendefoto, 4000);
     </script>
   </div>
 <div class="px-32 py-5">
@@ -26,17 +26,17 @@
         <!--!!-->
         <div class="w-3/12">
             <!-- 3 ảnh (để chạy) mẫu khác của sản phẩm -->
-            <button onclick="setImage1">
+            <button onclick="setImage1()">
                 <div>
                     <img id="image1" class="w-5/6  mt-4 mb-2 object-contain" src="{{asset("img/anh_giay_nam/male_shoes(1).png")}}" alt="" width="512" height="384">
                 </div>
             </button>
-            <button onclick="setImage2">
+            <button onclick="setImage2()">
                 <div>
                     <img id="image2" class="w-5/6  mt-4 mb-2 object-contain" src="{{asset("img/anh_giay_nam/male_shoes(6).png")}}" alt="" width="512" height="384">
                 </div>
             </button>
-            <button onclick="setImage3">
+            <button onclick="setImage3()">
                 <div> 
                     <img id="image3" class="w-5/6  mt-4 object-contain" src="{{asset("img/anh_giay_nam/male_shoes(11).png")}}" alt="" width="512" height="384">
                 </div>
@@ -47,7 +47,7 @@
             <img id="image" class="pl-2 pt-28 h-5/6 object-contain" src="{{asset("img/anh_giay_nam/male_shoes(16).png")}}" alt="">
         </div>
         <script>
-            var x = {{asset("img/anh_giay_nam/male_shoes(16).png")}};
+            var x = "{{asset("img/anh_giay_nam/male_shoes(16).png")}}";
             var a = document.getElementById("image");
             var b = document.getElementById("image1");
             var c = document.getElementById("image2");
@@ -55,17 +55,17 @@
             function setImage1() {
                 x = a.src;
                 a.src = b.src;
-                b.src=x;
+                b.src = x;
             }
             function setImage2() {
                 x = a.src;
                 a.src = c.src;
-                c.src=x;
+                c.src = x;
             }
             function setImage3() {
                 x = a.src;
                 a.src = d.src;
-                d.src=x;
+                d.src = x;
             }
         </script>
         <!-- Phần thông tin sản phẩm -->
@@ -142,123 +142,34 @@
         </div>
     </div>
     <!-- Phần đánh giá, mô tả -->
-    <div class="flex justify-between w-full h-96 mt-8 border-t-2 border-gray-300">
-        <div class="w-1/3 h-full mt-6 ml-4 border-r-2 border-gray-300">
-            <!-- Phần đánh giá -->
-            <div class="flex mr-2">
-                <div class="text-xl font-medium text-gray-700 w-2/3">
-                    Xếp hạng và đánh giá
-                </div>
-                <div class="flex w-1/3">
-                    <svg width="20" height="20" fill="currentColor" class="text-yellow-600">
-                        <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
-                    </svg>
-                    <div class="ml-1">
-                        <span class="text-black">4.94</span>
-                        <span class="sm:hidden md:inline">(128)</span>
-                    </div>
-                </div>
+    <div class="pt-10">
+        <div class="flex">
+            <div class="w-1/2">
+                <img class="" src="{{asset("img/anh_giay_nam/male_shoes(21).png")}}">
             </div>
-            <br>
-            <!-- Phần đánh giá khách hàng -->
-            <div class="flex text-sm font-medium text-gray-700 w-2/3">
-                20/07/2020 <!-- Ngày đánh giá -->
-                <div class="ml-32 flex">
-                    <svg width="20" height="20" fill="currentColor" class="text-yellow-600 mr-0">
-                        <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
-                    </svg>
-                    <div class="ml-1">
-                        <span class="text-black">4.5</span> <!-- Vote ../5 -->
-                    </div>
-                </div>
-            </div>
-            <div class="col-start-1 row-start-3 space-y-3 pr-4 pt-2"> <!-- Thông tin người mua -->
-                <p class="flex items-center text-black text-sm font-medium">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBWMw05M04BIRa7SIuc9f4by2Sa0VR7ZcFw&usqp=CAU" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
-                    Trần Xuân Hương
-                </p>
-            </div>
-            <p class="border-b-2 border-gray-500 w-2/3">Sản Phẩm dùng tốt</p> <!-- Comment -->
-            <div class="flex text-sm font-medium text-gray-700 w-2/3">
-                26/03/2021
-                <div class="ml-32 flex">
-                    <svg width="20" height="20" fill="currentColor" class="text-yellow-600 mr-0">
-                        <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
-                    </svg>
-                    <div class="ml-1">
-                        <span class="text-black">4.0</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-start-1 row-start-3 space-y-3 pr-4 pt-2">
-                <p class="flex items-center text-black text-sm font-medium">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBWMw05M04BIRa7SIuc9f4by2Sa0VR7ZcFw&usqp=CAU" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
-                    Lê Hưng
-                </p>
-            </div>
-            <p class="border-b-2 border-gray-500 w-2/3">Sản Phẩm dùng tạm được</p>
-            <div class="flex text-sm font-medium text-gray-700 w-2/3">
-                12/06/2021
-                <div class="ml-32 flex">
-                    <svg width="20" height="20" fill="currentColor" class="text-yellow-600 mr-0">
-                        <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
-                    </svg>
-                    <div class="ml-1">
-                        <span class="text-black">5.0</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-start-1 row-start-3 space-y-3 pr-4 pt-2">
-                <p class="flex items-center text-black text-sm font-medium">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBWMw05M04BIRa7SIuc9f4by2Sa0VR7ZcFw&usqp=CAU" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
-                    Lê Minh Tiến
-                </p>
-            </div>
-            <p class="border-b-2 border-gray-500 w-2/3">Amazingg goodd chóp</p>
-            <div class="flex text-sm font-medium text-gray-700 w-2/3">
-                13/05/2021
-                <div class="ml-32 flex">
-                    <svg width="20" height="20" fill="currentColor" class="text-yellow-600 mr-0">
-                        <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
-                    </svg>
-                    <div class="ml-1">
-                        <span class="text-black">4.5</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-start-1 row-start-3 space-y-3 pr-4 pt-2">
-                <p class="flex items-center text-black text-sm font-medium">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBWMw05M04BIRa7SIuc9f4by2Sa0VR7ZcFw&usqp=CAU" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
-                    Lê Ngọc Văn
-                </p>
-            </div>
-            <p class="">Sản phẩm nên mua</p>
-        </div>
-        <div class="w-2/3 h-full mt-4">
-            <div class="flex h-1/2 w-full">
-                <div class="h-full w-1/3"> <!-- Để ảnh sp -->
-                    <img class="w-5/6 h-full float-right" src="{{asset("img/anh_giay_nam/male_shoes(20).png")}}">
-                </div>
-                <!-- Phần giới thiệu -->
-                <div class="ml-4 mr-6 h-full w-2/3">
+            <div class="w-1/2">
+                <div class="pl-5">
                     <div>
-                        <h1 class="text-3xl font-semibold float-left"> NIKE FAKE</h1>
+                        <h1 class="text-3xl font-semibold float-left"> GIÀY STAN SMITH
+                            STAN SMITH, MÃI MÃI. ĐẬM CHẤT BIỂU TƯỢNG. GIỜ ĐÂY CÀNG BỀN VỮNG HƠN.
+                            </h1>
                     </div>
                     <div>
-                        <h3 class="text-l font-semibold float-rights w-full mr-48 mt-12">
-                            Tuy là hàng fake nhưng mà chất lượng không kém hàng real, màu sắc mẫu mã phải nói là chuẩn fake :)))
-                            <br> Sản phẩm đảm bảo 1 lần nhìn qua k thể phát hiện là giả :)) 
+                        <h3 class="text-sm font-semibold float-rights w-full">
+                            Vẻ đẹp kinh điển. Phong cách vốn dĩ. Đa năng hàng ngày. 
+                            Suốt hơn 50 năm qua và chưa dừng ở đó, giày adidas Stan Smith luôn giữ vững vị trí là một biểu tượng. 
+                            Đôi giày này là phiên bản cải biên mới mẻ, thể hiện cam kết của adidas hướng tới chỉ sử dụng polyester tái chế bắt đầu từ năm 2024. 
+                            Thêm vào đó, đế ngoài làm từ cao su phế liệu càng tôn lên phong cách kinh điển.   
+                            Sản phẩm này may bằng vải công nghệ Primegreen, thuộc dòng chất liệu tái chế hiệu năng cao. 
+                            Thân giày chứa 50% thành phần tái chế. Không sử dụng polyester nguyên sinh.
                         </h3>
                     </div>
                 </div>
-            </div>
-            <div class="flex h-1/2 w-full">
-                <!-- Phần thông số sản phẩm -->
-                <div class="h-full w-2/3 pt-6">
+                <div class="pt-20 pl-5">
                     <div class="h-1/6 w-1/3 pr-4 text-xl font-medium text-gray-700 ml-48 border-b-2 text-center border-gray-500">
                         Thông số
                     </div>
-                    <div class="flex h-4/6 w-full mt-6 pl-12">
+                    <div class="flex">
                         <div class="h-full w-1/2 ml-4 list-inside">
                             <li>Dánh regular fit</li>
                             <li>Có dây buộc</li>
@@ -271,6 +182,115 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="flex">
+            <div class="w-1/2">
+                <div class="flex mr-2 mt-4">
+                    <div class="text-xl font-medium text-gray-700 w-2/3">
+                        Xếp hạng và đánh giá
+                    </div>
+                    <div class="flex w-1/3">
+                        <svg width="20" height="20" fill="currentColor" class="text-yellow-600">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                        <div class="ml-1">
+                            <span class="text-black">4.94</span>
+                            <span class="sm:hidden md:inline">(128)</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex text-sm font-medium text-gray-700 w-2/3 mt-2">
+                    20/07/2020 <!-- Ngày đánh giá -->
+                    <div class="ml-32 flex">
+                        <svg width="20" height="20" fill="currentColor" class="text-yellow-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                        <div class="ml-1">
+                            <span class="text-black">4.5</span> <!-- Vote ../5 -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-start-1 row-start-3 space-y-3 pr-4 pt-2"> <!-- Thông tin người mua -->
+                    <p class="flex items-center text-black text-sm font-medium">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBWMw05M04BIRa7SIuc9f4by2Sa0VR7ZcFw&usqp=CAU" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
+                        Trần Xuân Hương
+                    </p>
+                </div>
+                <p class="border-b-2 border-gray-500 w-2/3">Sản Phẩm dùng tốt</p> <!-- Comment -->
+                <div class="flex text-sm font-medium text-gray-700 w-2/3 mt-2">
+                    26/03/2021
+                    <div class="ml-32 flex">
+                        <svg width="20" height="20" fill="currentColor" class="text-yellow-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                        <div class="ml-1">
+                            <span class="text-black">4.0</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-start-1 row-start-3 space-y-3 pr-4 pt-2">
+                    <p class="flex items-center text-black text-sm font-medium">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBWMw05M04BIRa7SIuc9f4by2Sa0VR7ZcFw&usqp=CAU" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
+                        Lê Hưng
+                    </p>
+                </div>
+                <p class="border-b-2 border-gray-500 w-2/3">Sản Phẩm dùng tạm được</p>
+                <div class="flex text-sm font-medium text-gray-700 w-2/3 mt-2">
+                    12/06/2021
+                    <div class="ml-32 flex">
+                        <svg width="20" height="20" fill="currentColor" class="text-yellow-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                        <div class="ml-1">
+                            <span class="text-black">5.0</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-start-1 row-start-3 space-y-3 pr-4 pt-2">
+                    <p class="flex items-center text-black text-sm font-medium">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBWMw05M04BIRa7SIuc9f4by2Sa0VR7ZcFw&usqp=CAU" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
+                        Lê Minh Tiến
+                    </p>
+                </div>
+                <p class="border-b-2 border-gray-500 w-2/3">Amazingg goodd chóp</p>
+                <div class="flex text-sm font-medium text-gray-700 w-2/3 mt-2">
+                    13/05/2021
+                    <div class="ml-32 flex">
+                        <svg width="20" height="20" fill="currentColor" class="text-yellow-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                        <div class="ml-1">
+                            <span class="text-black">4.5</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-start-1 row-start-3 space-y-3 pr-4 pt-2">
+                    <p class="flex items-center text-black text-sm font-medium">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdBWMw05M04BIRa7SIuc9f4by2Sa0VR7ZcFw&usqp=CAU" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
+                        Lê Ngọc Văn
+                    </p>
+                </div>
+                <p class="">Sản phẩm nên mua</p>
+            </div>
+            <div class="w-1/2 float-left">
+                <img class="" src="{{asset("img/anh_giay_nam/male_shoes(21).png")}}">
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-between w-full h-96 mt-8 border-t-2 border-gray-300">
+        <div class="w-1/3 h-full mt-6 ml-4 border-r-2 border-gray-300">
+        </div>
+        <div class="w-2/3 h-full mt-4">
+            <div class="flex h-1/2 w-full">
+                <div class="h-full w-1/3"> <!-- Để ảnh sp -->
+                    <img class="w-5/6 h-full float-right" src="{{asset("img/anh_giay_nam/male_shoes(20).png")}}">
+                </div>
+                
+            </div>
+            <div class="flex h-1/2 w-full">
+                <!-- Phần thông số sản phẩm -->
+                
                 <div class="h-full w-1/3"> <!-- Để ảnh sp -->
                     <img class="w-5/6 h-full float-right mr-12" src="{{asset("img/anh_giay_nam/male_shoes(21).png")}}">
                 </div>
