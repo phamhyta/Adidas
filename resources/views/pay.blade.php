889d54a1840325a24fbd666679cd19b39c6bb0d5
Tran Xuan Huong <phamhyta@gmail.com>
2021-08-31 21:11:22 +0700

    --- Auto-Git Commit ---

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -1,22 +1,38 @@
 @extends('layout.header')
 @section('content')
-<div class="h-96 flex">
-<div class="w-2/3 border-2"></div>
-<div class="w-1/3 border-2 bg-purple-300 px-20">
-    <div class="">ORDER SUMARY</div>
-    <div class="flex">
-        <div class="w-1/2">Subtotal</div>
-        <div class="w-1/2 text-sm font-semibold text-white pl-2 line-through relative float-left">200$</div>  
+<div class="h-96 flex px-32">
+    <div class="w-3/4 border-2 pr-32">
+        <div class="w-full">
+            <div class="py-3 pl-10 bg-black text-2xl text-white">
+                Delivery
+            </div>
+            <div>
+                <div class="w-1/2">
+                    <input type="text" class="w-full border-b-2">
+                </div>
+                <div class="w-1/2">
+                    <input type="text" class="w-full border-b-2">
+                </div>
+            </div>
+        </div>
+        <div class="w-full"></div>
+        <div class="w-full"></div>
     </div>
-    <div class="flex">
-        <div class="w-1/2">Delivery</div>
-        <div class="w-1/2 float-right">Free</div>
+    <div class="w-1/4 bg-purple-100 px-10">
+        <div class="pt-6">ORDER SUMARY</div>
+        <div class="flex pt-6">
+            <div class="w-1/2">Subtotal</div>
+            <div class="w-1/2 text-right">200$</div>  
+        </div>
+        <div class="flex py-6">
+            <div class="w-1/2">Delivery</div>
+            <div class="w-1/2 text-right">Free</div>
+        </div>
+        <div class="flex py-6 border-t-2 border-black mb-6">
+            <div class="w-1/2">Total</div>
+            <div class="w-1/2 text-right">200$</div>
+        </div>
+        <button class="w-full py-2 mb-2 items-center justify-center rounded-md bg-black text-white hover:bg-gray-800" type="submit">Checkout</button>
     </div>
-    <div class="flex">
-        <div class="w-1/2">Total</div>
-        <div class="w-1/2 float-right">200$</div>
-    </div>
-    <button class="bg-black text-white w-full">Checkout</button>
-</div>
 </div>
 @endsection
