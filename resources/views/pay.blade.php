96cc2da60deac26477012b4df25570fdbd1ed744
Tran Xuan Huong <phamhyta@gmail.com>
2021-08-31 22:11:46 +0700

    --- Auto-Git Commit ---

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -68,7 +68,7 @@
                 <input type="text" class="w-full border-b-2 border-gray-500 outline-none" placeholder="Promo code">
             </div>
             <div class="pl-10 pt-5 w-1/2">Select payment menthod</div>
-            <div class="pl-10 pt-5 flex">
+            <div class="pl-10 pt-5 flex pb-10">
                 <div class="pr-6 flex w-1/2">
                     <div class="w-2/3">Credit or Debit card</div>
                     <div class="w-1/3 text-right">
@@ -82,8 +82,74 @@
                     </div>
                 </div>
             </div>
+            <div class="h-32 pl-10">
+                <div class="bg-purple-100 h-full">
+                    <div class="flex">
+                        <div class="w-1/3 pl-5 pt-4">
+                            <div class="pl-1">Card number</div>
+                            <div>
+                                <input type="text" class="border-2 outline-none">
+                            </div>
+                        </div>
+                        <div class="w-1/3 pl-5 pt-4">
+                            <div class="pl-1">Expire date</div>
+                            <div>
+                                <input type="text" class="border-2 outline-none">
+                            </div>
+                        </div>
+                        <div class="w-1/3 pl-5 pt-4">
+                            <div class="pl-1">Secure code</div>
+                            <div>
+                                <input type="text" class="w-5/6 border-2 outline-none">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex pl-5 pt-4">
+                        <div> 
+                            <input type="checkbox">
+                        </div>
+                        <div class="pl-4">Save for later use</div>
+                    </div>
+                </div>
+            </div> 
         </div>
-        <div class="w-full"></div>
+        <div class="w-full pt-12">
+            <div class="py-3 pl-10 bg-black text-2xl text-white">
+                Write your felling
+            </div>
+            <div class="pl-10 pt-4">Rate us</div>
+            <div class="pl-10 flex">
+                <div>
+                    <svg width="30" height="30" fill="currentColor" class="text-gray-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                </div>
+                <div>
+                    <svg width="30" height="30" fill="currentColor" class="text-gray-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                </div>
+                <div>
+                    <svg width="30" height="30" fill="currentColor" class="text-gray-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                </div>
+                <div>
+                    <svg width="30" height="30" fill="currentColor" class="text-gray-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                </div>
+                <div>
+                    <svg width="30" height="30" fill="currentColor" class="text-gray-600 mr-0">
+                            <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
+                        </svg>
+                </div>
+            </div>
+            <div class="pl-10 pt-2">Comment</div>
+            <div class="pl-10">
+                <input type="text" class="border-2 outline-none w-full h-12">
+            </div>
+        </div>
     </div>
     <div class="w-1/4 bg-purple-100 px-10">
         <div class="pt-6">ORDER SUMARY</div>
