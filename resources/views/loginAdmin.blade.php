57222309f2c69f198ee0b047e67e37d9101fcd3a
Tran Xuan Huong <phamhyta@gmail.com>
2021-09-08 22:47:21 +0700

    --- Auto-Git Commit ---

--- resources/views/loginAdmin.blade.php
+++ resources/views/loginAdmin.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.header')
 @section('content')
 {{-- slider --}}
-<div class="w-full h-96">
+<div class="w-full">
     <img id="img" class="w-full h-full" src="https://giayhiendai.com/upload/hinhanh/slider-19801.jpg" alt="">
     <script type="text/javascript">
         var img = document.getElementById("img");
@@ -17,23 +17,25 @@
         setInterval(volgendefoto, 5000);
     </script>
 </div>
-<div class="h-96 flex">
+<div class="flex py-20">
     <div class="w-1/3"></div>
-    <div class="w-1/3">
-        <div class="pt-12"><h1 class="px-20 pt- 0 text-center text-xl">Login Here</h1></div>
-        <div class="pt-3 pl-20">
+    <div class="w-1/3 bg-gray-100">
+        <div class="pt-2 bg-gray-300"><h1 class="px-20 pt- 0 text-center text-xl">Login Here</h1></div>
+        <div class="pt-3 px-16">
             <form>
             <p>Username</p>
-            <input type="text" name="" placeholder="Enter Username" value="">
+            <input class="border-2 px-2 py-1 w-full outline-none rounded-md" type="text" name="" placeholder="Enter Username" value="">
             <p>Password</p>
-            <input type="password" name="" placeholder="Enter Password" value="">
+            <input class="border-2 px-2 py-1 w-full outline-none rounded-md" type="password" name="" placeholder="Enter Password" value="">
             </form>
         </div> 
-        <div>
-            <input type="submit" name="" value="Login">
+        <div class="text-center pt-4">
+            <input class="bg-black text-white py-1 px-8 rounded-md hover:bg-gray-900" type="submit" name="" value="Login">
         </div> 
-        <a>Lost your password?</a><br>    
+        <div class="pl-16 py-4">
+             <a>Lost your password?</a><br>   
+        </div>
+       
     </div>
-    
 </div>
 @endsection
